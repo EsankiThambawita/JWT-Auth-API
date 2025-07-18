d859e30d40cb1e5ac07ba19e34d2a377a5494635
Esanki Thambawita <esankilakvindee2000@gmail.com>
2025-07-18 21:21:06 +0530

    Created the UserRepository

---
+++ src/main/java/com/example/JwtAuth/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.JwtAuth.repositories;
+
+
+import com.example.JwtAuth.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
